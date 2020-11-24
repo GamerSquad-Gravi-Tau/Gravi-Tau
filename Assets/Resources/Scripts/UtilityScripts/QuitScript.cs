4294669e0414e9ecbafecc5c17ef1230f6030611
Kyle Gersbach <Gersbach.Ka@gmail.com>
2020-11-23 17:13:19 -0800

    Fixed some menu stuff

--- Assets/Resources/Scripts/UtilityScripts/QuitScript.cs
+++ Assets/Resources/Scripts/UtilityScripts/QuitScript.cs
@@ -7,7 +7,7 @@ public class QuitScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.Q)){
+        if(Input.GetKey(KeyCode.Escape)){
             Application.Quit();
         }
     }

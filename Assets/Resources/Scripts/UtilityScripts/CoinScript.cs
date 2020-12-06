5a781a38d4ca086c3e3f1672c491f139482a2c14
Daniel Penkov <danielpenkov@daniels-mbp.lan>
2020-12-05 17:47:10 -0800

    refactored coin drop scripts

--- Assets/Resources/Scripts/UtilityScripts/CoinScript.cs
+++ Assets/Resources/Scripts/UtilityScripts/CoinScript.cs
@@ -5,21 +5,20 @@ using UnityEngine.UI;
 
 public class CoinScript : MonoBehaviour
 {
-    public static int count;
+    public static int PlayerWallet;
     public Text coinCount;
-
-    public GameObject player;
 
     private void Start()
     {
-        player = GameObject.Find("PlayerShip");
-        count = player.GetComponent<PlayerCoin>().PlayerWallet;
-        coinCount.text = "" + count;
+        coinCount.text = "" + PlayerWallet;
     }
     private void Update()
     {
-        player = GameObject.Find("PlayerShip");
-        count = player.GetComponent<PlayerCoin>().PlayerWallet;
-        coinCount.text = "" + count;
+        coinCount.text = "" + PlayerWallet;
+    }
+
+    public static void addCoins(int coins)
+    {
+        PlayerWallet += coins;
     }
 }
